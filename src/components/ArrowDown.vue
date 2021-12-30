2d3f28c010d6a6a2629d6ccbd6042f448c691078
Lucas de Brito <lucas.debrito@lexartlabs.com>
2021-12-29 22:37:01 -0300

    arrow down

---
+++ src/components/ArrowDown.vue
@@ -0,0 +1,30 @@
+<template>
+    <img
+        class="see-more-arrow"
+        src="https://img.icons8.com/ios/32/ffffff/expand-arrow--v2.png"
+    />
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.see-more-arrow {
+  margin: 20px auto 0 auto;
+  display: block;
+  animation-name: seeMoreArrow;
+  animation-duration: 4s;
+  animation-iteration-count: infinite;
+}
+
+@keyframes seeMoreArrow {
+  0% {
+    transform: translateY(0px)
+  } 25% {
+    transform: translateY(5px)
+  } 100% {
+    transform: translateY(0px)
+  }
+}
+</style>
